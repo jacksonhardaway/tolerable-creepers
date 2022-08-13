662d8aedb0a90f663704de269cc76274df240e60
Ocelot <FinntheRaider@gmail.com>
2022-08-13 13:53:33 -0600

    Upgraded to 1.18.2

--- common/src/main/java/gg/moonflower/tolerablecreepers/core/registry/TCTags.java
+++ common/src/main/java/gg/moonflower/tolerablecreepers/core/registry/TCTags.java
@@ -4,15 +4,16 @@ import gg.moonflower.pollen.api.registry.resource.TagRegistry;
 import gg.moonflower.tolerablecreepers.core.TolerableCreepers;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Block;
 
 public class TCTags {
 
-    public static Tag.Named<EntityType<?>> EXPLOSION_IMMUNE = TagRegistry.bindEntityType(new ResourceLocation(TolerableCreepers.MOD_ID, "explosion_immune"));
-    public static Tag.Named<EntityType<?>> EXPLOSION_PRONE = TagRegistry.bindEntityType(new ResourceLocation(TolerableCreepers.MOD_ID, "explosion_prone"));
+    public static TagKey<EntityType<?>> EXPLOSION_IMMUNE = TagRegistry.bindEntityType(new ResourceLocation(TolerableCreepers.MOD_ID, "explosion_immune"));
+    public static TagKey<EntityType<?>> EXPLOSION_PRONE = TagRegistry.bindEntityType(new ResourceLocation(TolerableCreepers.MOD_ID, "explosion_prone"));
 
-    public static Tag.Named<EntityType<?>> CREEPIE_AVOID = TagRegistry.bindEntityType(new ResourceLocation(TolerableCreepers.MOD_ID, "creepie_avoid"));
+    public static TagKey<EntityType<?>> CREEPIE_AVOID = TagRegistry.bindEntityType(new ResourceLocation(TolerableCreepers.MOD_ID, "creepie_avoid"));
 
-    public static Tag.Named<Block> CREEPIE_REPELLENTS = TagRegistry.bindBlock(new ResourceLocation(TolerableCreepers.MOD_ID, "creepie_repellents"));
+    public static TagKey<Block> CREEPIE_REPELLENTS = TagRegistry.bindBlock(new ResourceLocation(TolerableCreepers.MOD_ID, "creepie_repellents"));
 }
